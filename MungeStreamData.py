efb4d01c0e6fc2806a826f7e7387f72360d6508c
Mike Kelly <mike@tech-dna.net>
2017-12-23 10:43:03 -0800

    First version that accepts right parameters

--- MungeStreamData.py
+++ MungeStreamData.py
@@ -21,86 +21,57 @@ filesToExclude = ['.dropbox', 'desktop.ini' ]
 # If any one missing, it is skipped
 dirsToInclude = ['Artifacts', 'Surveyor', 'To-From Client', 'StaticAnalysis']
 
-# Locate Dropbox folder
-# From https://stackoverflow.com/questions/12118162/how-can-i-get-the-dropbox-folder-location-programmatically-in-python
-def getDropboxFolderPath():
-    try:
-        jsonPath = (Path(os.getenv('LOCALAPPDATA'))/'Dropbox'/'info.json').resolve()
-    except FileNotFoundError:
-        jsonPath = (Path(os.getenv('APPDATA'))/'Dropbox'/'info.json').resolve()
-    
-    with open(str(jsonPath)) as f:
-        j = json.load(f)
-
-    # os.path.join adds trailing slash if not there    
-    businessDropboxFolderRoot = os.path.join(Path(j['business']['path']), '')
-    print('Looking in Dropbox at', businessDropboxFolderRoot)
-    return businessDropboxFolderRoot
-    
-# Locate files to include in the archive
-def collectFiles(businessDropboxFolderRoot, projectName):
-    filesToZip = []
-    projectRoot = businessDropboxFolderRoot + projectName
-    for subdir, dirs, files in os.walk(projectRoot):
+# Locate files to process
+def collectFiles(inputFolder, outputFolder):
+    filesToRead = []
+    for subdir, dirs, files in os.walk(inputFolder):
         for file in files:
             if file not in filesToExclude:
                 # Check if this file matches one of the directories we want to include
                 # skip others, e.g. To-From Target
                 if len([x for x in dirsToInclude if subdir.find('\\'+x)>0])>0:
                     print('Adding '+os.path.join(subdir, file))
-                    filesToZip.append('"'+os.path.join(subdir, file)+'"')
-    return filesToZip
+                    filesToRead.append('"'+os.path.join(subdir, file)+'"')
+    return filesToRead
         
 
-# Zip up files
-# Returns CompletedProcess with output
-def sevenzipWindows(files, zipname, password):
-    
-    # Base zip command with name of archive and password
-    cmd = ["7z", "a", "-p{}".format(password), '--', zipname.title()]
-    # Append the list of files to add to the ZIP archive
-    fd, path = mkstemp()
-    # use a context manager to open the file at that path and close it again
-    with open(path, 'w') as f:
-        f.write('\n'.join(files))
-
-    os.close(fd)
-    cmd.append('@'+path)
-    print('7Zip execution:\n', cmd)
-    return subprocess.run(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, universal_newlines=True)
-
 def helpMessage():
-      print('techdna-archive.py -p <project>')
-      print('Creates archive for project in Tech DNA Dropbox / Project Archives')
+      print('MungeStreamData.py [-o <outputFolder>] -i <inputFolder>')
+      print('Processes all data files under <inputFolder>')
+      print('Output goes to specified output folder, default is ProcessedStreamData')
       sys.exit(2)
 
 
 def main(argv):
-   projectName = ''
    
    # Only supporting Windows for now...
    if (platform.system() != 'Windows'):
         print('Sorry, this script is supported only on Windows for now... bug Mike')
         sys.exit(2)
-        
-   dropboxFolderRoot = getDropboxFolderPath()
+
+   # Default output folder 
+   outputFolder = 'ProcessedStreamData'
+   inputFolder = '.'
 
    try:
-      opts, args = getopt.getopt(argv,"hp:",["project="])
+      opts, args = getopt.getopt(argv,"iho:",["input=", "help", "output="])
    except getopt.GetoptError:
       helpMessage()
    for opt, arg in opts:
       if opt == '-h':
          helpMessage()
-      elif opt in ("-p", "--project"):
-         projectName = arg
+      elif opt in ("-o", "--output:"):
+         outputFolder = arg
+      elif opt in ("-i", "--input:"):
+         inputFolder = arg
 
-   print('Archiving "'+projectName+'"...')
+   if inputFolder == '':
+         helpMesssage()
+
+   print('Processing data in "'+ inputFolder+ '"...')
    
-   files = collectFiles(dropboxFolderRoot, projectName)
-   # TO DO - generate password
-   SevenZipStatus = sevenzipWindows(files, os.path.join(dropboxFolderRoot, 'Project Archives', projectName + ' Project Archive.zip'), '12345')
-   print (SevenZipStatus.stdout)
+   files = collectFiles(inputFolder, outputFolder)
+   print(files)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
--- README.md
+++ README.md
@@ -1,31 +1,30 @@
-* KCStreamDataTool
-Tool for munging KC Stream Data 
+# KCStreamDataTool - Tool for munging KC Stream Data 
 
-Mini Spec for Data Massaging
+# Mini Spec for Data Massaging
 
-#Current situation
+## Current situation
 HI-9829 Data is located in a non-organized folder tree with general name of "LOG0XX_#########"
 Each file has two sheets
-* Sheet 1, "Lot Info" has the measurement information
-  * Most importantly, it includes lot names, which are the site names
-  * For example, Yelac = YELAC, and Rutzpond = RUTZP
-  * Some of the lot names are wrong (e.g. WHISI should be WHIRU)
-  * Lot names are located in cell B19
-* Sheet 2, "Log data - 1", contains a series of measurements for a given day
-  * Sometimes the days are "on top" of each other in the same column
-  * Sometimes the file has the data for a single day
-  * Sometimes the data has the data for a single day, but for some reason the first several hundred rows are empty
+- Sheet 1, "Lot Info" has the measurement information
+  - Most importantly, it includes lot names, which are the site names
+  - For example, Yelac = YELAC, and Rutzpond = RUTZP
+  - Some of the lot names are wrong (e.g. WHISI should be WHIRU)
+  - Lot names are located in cell B19
+- Sheet 2, "Log data - 1", contains a series of measurements for a given day
+  - Sometimes the days are "on top" of each other in the same column
+  - Sometimes the file has the data for a single day
+  - Sometimes the data has the data for a single day, but for some reason the first several hundred rows are empty
 #What I want
 1. First, I want the raw data files organized: one file per site per measurement day
-  * Named as "SITENAME_YYYY-MM-DD" (e.g. YELAC_2017-10-01)
-  * These files should be put in the correct folders (either by measurement days or sites?)
-  * _OPEN ISSUE:_ Do I want all the measurements from YELAC together or all the measurements from 10-01-17 together?
-  * Then, I want the median measurement for each parameter per site per measurement day (that is to say, I want one value for dissolved oxygen at YELAC for 10-01-17)
-      * This means taking the median for a given day
-  * Maybe I make third sheet which "summarizes" the data by giving me a median value for the parameters as well as date and time
-  * Lastly, I want all of the median values put into my summary/master spreadsheet
-*	Each site has its own sheet
-*	I want to populate the tables with median values from each date/time and parameter
-*	From there, I will prepare the EIM files?
-*	Eventually, I think I will need each parameter for each site to be in its own EIM-formatted CSV which we can then upload to DOE
+  - Named as "SITENAME_YYYY-MM-DD" (e.g. YELAC_2017-10-01)
+  - These files should be put in the correct folders (either by measurement days or sites?)
+  - _OPEN ISSUE:_ Do I want all the measurements from YELAC together or all the measurements from 10-01-17 together?
+  - Then, I want the median measurement for each parameter per site per measurement day (that is to say, I want one value for dissolved oxygen at YELAC for 10-01-17)
+      - This means taking the median for a given day
+  - Maybe I make third sheet which "summarizes" the data by giving me a median value for the parameters as well as date and time
+  - Lastly, I want all of the median values put into my summary/master spreadsheet
+Each site has its own sheet
+ - I want to populate the tables with median values from each date/time and parameter
+_OPEN ISSUE:_ From there, I will prepare the EIM files?
+Eventually, I think I will need each parameter for each site to be in its own EIM-formatted CSV which we can then upload to DOE
 
